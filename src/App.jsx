fcc6e52048ee80f5d22dba0ce24a14277abd871f
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-12-30 19:13:37 -0500

    build(project): :heavy_plus_sign: add tailwind and react-router

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => (
+	<div className='p-4'>
+		<h1>Esteban Salazar</h1>
+		<img src='/vite.svg' />
+	</div>
+);
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
+import App from './App';
+import './styles/index.css';
 
-const app = (
-	<div>
-		<h1>Esteban Salazar</h1>
-		<img src='/vite.svg' />
-	</div>
-);
-const container = document.getElementById('root');
-
-createRoot(container).render(app);
+createRoot(document.getElementById('root')).render(<App />);
